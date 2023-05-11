f604ce9111657755b1d3458f0094f9f7641f9350
jluevan13 <luevano.jesus13@gmail.com>
2023-05-11 16:34:38 -0400

    remove echo

--- deployment_files/scanImage.sh
+++ deployment_files/scanImage.sh
@@ -8,7 +8,7 @@ docker pull bkimminich/juice-shop:latest
 ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details bkimminich/juice-shop -o output.json
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
 
-echo "Rule set to Fail on Critical Vulnerabilities"
+# echo "Rule set to Fail on Critical Vulnerabilities"
 # echo "Rule set to only Alert"
 
 # if [ -z "$result" ] || [ $BYPASS_IMAGESCAN == 1 ]; then
