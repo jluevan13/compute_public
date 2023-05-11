06ee57cf9d96005133816fe953a3699dc4c480e7
jluevan13 <luevano.jesus13@gmail.com>
2023-05-11 16:50:06 -0400

    add labels

--- deployment_files/scanImage.sh
+++ deployment_files/scanImage.sh
@@ -6,11 +6,11 @@ chmod a+x ./twistcli
 docker pull bkimminich/juice-shop:latest     
 #curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
 ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details bkimminich/juice-shop -o output.json
-result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
+result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=juice-shop&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
 
 # echo "Rule set to Fail on Critical Vulnerabilities"
 # echo "Rule set to only Alert"
-# echo $result
+echo "$result"
 
 # if [ -z "$result" ] || [ $BYPASS_IMAGESCAN == 1 ]; then
 #    echo "Image Vulnerability scan passed!"
